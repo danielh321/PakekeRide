75ef1f79c6539c953bf76ee117ba523d0f1c5531
danielh321 <104436860+danielh321@users.noreply.github.com>
2023-04-30 15:08:36 +0300

    Add files via upload

---
+++ Nav.js
@@ -0,0 +1,43 @@
+const nav = document.querySelector('nav');
+
+window.addEventListener('scroll', ()=>{
+    if (window.scrollY>=50){
+        nav.classList.add('active_nav');
+    }else{
+        nav.classList.remove('active_nav');
+    }
+
+});
+
+const observer1 = new IntersectionObserver((entries) =>{
+    entries.forEach((entry)=>{
+        if (entry.isIntersecting){
+            entry.target.classList.add('show')
+        }else {
+            entry.target.classList.remove('show')
+        }
+    });
+});
+
+const hiddenH = document.querySelectorAll('.content2');
+hiddenH.forEach((el)=> observer1.observe(el));
+
+
+
+
+const observer = new IntersectionObserver((entries) =>{
+    entries.forEach((entry)=>{
+        if (entry.isIntersecting){
+            entry.target.classList.add('show2')
+        } else {
+            entry.target.classList.remove('show2')
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.content');
+hiddenElements.forEach((el)=> observer.observe(el));
+
+
+
+
